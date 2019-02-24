264b927d32f94cf0f09950f85e7dcc218d0c5a69
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-02-24 19:33:15 -0300

    Criando as views (index e delete) para as Questões

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins  #Administradores
+    resources :admins  # Administradores
     resources :subjects # Assuntos/Areas
+    resources :questions # Perguntas
   end
   
   devise_for :admins
